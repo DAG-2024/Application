dc73e135326c6f2ecb0edc9e8c23780c8b488a25
amitadm <168081888+amitadm@users.noreply.github.com>
2025-08-20 16:19:17 +0300

    Update stitcherModels.py
    added List of wordToken objects -> json function and json -> list of wordToken objects

--- src/stitcher/models/stitcherModels.py
+++ src/stitcher/models/stitcherModels.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import List, Optional
-
-
+from pydantic import TypeAdapter
+import json
 
 class WordToken(BaseModel):
     start: float
@@ -10,6 +10,19 @@ class WordToken(BaseModel):
     to_synth: bool
     is_speech: bool
     synth_path: Optional[str] = None  # filled in after TTS
+
+def wordtokens_to_json(wordtokens: List[WordToken]) -> str:
+    """
+    Serialize a list of WordToken objects to a JSON string.
+    """
+    return json.dumps([w.model_dump() for w in wordtokens])
+
+def wordtokens_from_json(json_str: str) -> List[WordToken]:
+    """
+    Deserialize a JSON string into a list of WordToken objects.
+    """
+    data = json.loads(json_str)
+    return TypeAdapter(List[WordToken]).validate_python(data)
 
 
 class FixResponse(BaseModel):
